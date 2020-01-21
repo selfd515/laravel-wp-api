93165dcccaad69646048940b83ab670dda478aad
yuliagolovanova <selfd.515@gmail.com>
2020-01-21 18:25:33 +0400

    Added media by id request

--- src/Swancreative/LaravelWpApi/WpApi.php
+++ src/Swancreative/LaravelWpApi/WpApi.php
@@ -89,7 +89,7 @@ class WpApi
      */
     public function post($slug)
     {
-        return $this->get('posts', ['slug' => $slug, '?_embed']);
+        return $this->get('posts', ['slug' => $slug, 'context' => 'embed']);
     }
 
     /**
@@ -148,7 +148,7 @@ class WpApi
      */
     public function categoryPosts($cat = null, $page = null, $pp = null)
     {
-        return $this->get('posts', ['categories' => trim($cat),'page' => $page, 'per_page' => $pp, '?_embed']);
+        return $this->get('posts', ['categories' => trim($cat),'page' => $page, 'per_page' => $pp, 'context' => 'embed']);
     }
 
     /**
